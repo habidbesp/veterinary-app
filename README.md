0c1abbb300c4e9fd886488cc2ede32a352b864d6
Habid B Espinosa <hab.bad@hotmail.com>
2024-10-06 10:14:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Veterinary App
+
+In Veterinary App, you can enter your pet's information, as well as a description of their symptoms. This information will help track the health of your beloved companion, facilitating better veterinary care and more effective monitoring of their well-being.
+
+## Features
+
+- Enter your pet's information and symptoms in a form.
+- Display a list of pet patients with their information and symptoms.
+
+## Technologies Used
+
+The project was developed using:
+
+- **React**: A JavaScript library for building user interfaces.
+- **Vite**: A fast build tool for modern web applications.
+- **TypeScript**: A typed superset of JavaScript that compiles to plain JavaScript.
+- **Zustand**: A lightweight state management library for React that simplifies global state management using hooks.
+- **Tailwind CSS**: A utility-first CSS framework for styling.
+
+## How to Run the Project
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/habidbesp/veterinary-app.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd veterinary-app
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+4. Run the project in development mode:
+
+   ```bash
+   npm run dev
+   ```
+
+5. Open in your browser: http://localhost:5173
+
+## Course Information
+
+This application was developed as part of the Udemy course:
+
+**"React: Hooks y State hasta TypeScript, Zod, Zustand, React Query, Next.js, React Router, MERN y PERN"**
+
+Taught by: _Juan Pablo De la Torre Valdez_  
+[More about the course](https://codigoconjuan.com/)
+
+## Contributions
+
+Contributions are welcome! If you have suggestions, ideas, or improvements, feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ src/components/PatientForm.tsx
@@ -0,0 +1,120 @@
+import { useForm } from "react-hook-form";
+import Error from "./Error";
+
+export default function PatientForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const registerPatient = () => {
+    console.log("new Patient");
+  };
+  return (
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
+      <h2 className="font-black text-3xl text-center">Patient Follow-up</h2>
+
+      <p className="text-lg mt-5 text-center mb-10">
+        Add Patients and {""}
+        <span className="text-indigo-600 font-bold">Manage Them</span>
+      </p>
+
+      <form
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+        noValidate
+        onSubmit={handleSubmit(registerPatient)}
+      >
+        <div className="mb-5">
+          <label htmlFor="name" className="text-sm uppercase font-bold">
+            Patient
+          </label>
+          <input
+            id="name"
+            className="w-full p-3  border border-gray-100"
+            type="text"
+            placeholder="Patient Name"
+            {...register("name", { required: "Patient name is required" })}
+          />
+          {errors.name && <Error>{errors.name?.message?.toString()}</Error>}
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="caretaker" className="text-sm uppercase font-bold">
+            Pet Owner
+          </label>
+          <input
+            id="caretaker"
+            className="w-full p-3  border border-gray-100"
+            type="text"
+            placeholder="Pet Owner"
+            {...register("caretaker", {
+              required: "Pet Owner's name is required",
+            })}
+          />
+          {errors.caretaker && (
+            <Error>{errors.caretaker?.message?.toString()}</Error>
+          )}
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="email" className="text-sm uppercase font-bold">
+            Email
+          </label>
+          <input
+            id="email"
+            className="w-full p-3  border border-gray-100"
+            type="email"
+            placeholder="Email"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Email is not valid",
+              },
+            })}
+          />
+          {errors.email && <Error>{errors.email?.message?.toString()}</Error>}
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="date" className="text-sm uppercase font-bold">
+            Date of Admission
+          </label>
+          <input
+            id="date"
+            className="w-full p-3  border border-gray-100"
+            type="date"
+            {...register("date", {
+              required: "Date of Admission is required",
+            })}
+          />
+          {errors.date && <Error>{errors.date?.message?.toString()}</Error>}
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="symptoms" className="text-sm uppercase font-bold">
+            Patient Symptoms
+          </label>
+          <textarea
+            id="symptoms"
+            className="w-full p-3  border border-gray-100"
+            placeholder="Patient Symptoms"
+            {...register("symptoms", {
+              required: "Symptoms are required",
+            })}
+          />
+          {errors.symptoms && (
+            <Error>{errors.symptoms?.message?.toString()}</Error>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+          value="Guardar Paciente"
+        />
+      </form>
+    </div>
+  );
+}

ad9669d4e4696c358d9285788744c2bc0267b9be
Habid B Espinosa <hab.bad@hotmail.com>
2024-10-07 18:14:34 +0200

    deletePatient function in store of zustand, filterfunction to eliminate a patient

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,9 +1,37 @@
 import { usePatientStore } from "../store";
+import PatientDetails from "./PatientDetails";
 
 export default function PatientsList() {
   const patients = usePatientStore((state) => state.patients);
 
-  console.log(patients);
-
-  return <div>PatientsList</div>;
+  return (
+    <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll">
+      {patients.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Patients List</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Manage your{" "}
+            <span className="text-indigo-600 font-bold">
+              patients and appointments.
+            </span>
+          </p>
+          {patients.map((item) => (
+            <PatientDetails key={item.id} patient={item} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">
+            There are not patients
+          </h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Start by adding patients,{" "}
+            <span className="text-indigo-600 font-bold">
+              and they will appear here.
+            </span>
+          </p>
+        </>
+      )}
+    </div>
+  );
 }
